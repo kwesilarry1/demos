294dae9bbf25160cfd02a45127d286b1072c45fa
kwesilarry <appiahbis999@gmail.com>
2026-01-19 21:03:26 +0000

    Mobile App demos

---
+++ dartpedia/cli/bin/cli.dart
@@ -0,0 +1,15 @@
+
+const version = '0.0.1'; 
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help'){
+  printUsage();
+  } else if (arguments.first == 'version'){
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search'){
+    print("Search Command Recognized");
+  }else {
+    printUsage();
+    }
+    } void printUsage(){
+      print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+      }
